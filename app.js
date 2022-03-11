1f1619de87a4e7c85af1b597257d710bb79a56d8
tonnyelongo <77832721+tonnyelongo@users.noreply.github.com>
2022-03-12 00:14:27 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,170 @@
+const express=require("express")
+const app=express()
+const session=require("express-session")
+var FileStore = require('session-file-store')(session);
+const multer=require("multer")
+const ejs=require("ejs")
+const bodyParser=require("body-parser")
+const path=require("path")
+const cors=require("cors")
+const fs = require('fs')
+var xss = require("xss")
+const admin=require("./router/admin")
+
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.use(session({ secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({logFn: function(){}}),
+    cookie: { maxAge: 36000000000,secure: false, httpOnly: true }
+  })
+);
+
+/*
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'almadb'
+});
+*/
+
+
+var storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"public/uploads")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.fieldname+"-"+Date.now()+file.originalname)
+    }
+})
+
+var upload=multer({storage:storage})
+
+app.set("views",__dirname+"/views")
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"build")))
+
+
+/*if (process.env.NODE_ENV === "production") {
+	app.get("/",(req,res)=>{
+		app.use(express.static(__dirname+"/build"))
+		res.sendFile(path.join(__dirname+"/build/index.html"))
+	})
+
+}*/
+/*app.get("/",(req,res)=>{
+	app.use(express.static(__dirname+"/build"))
+	res.sendFile(path.join(__dirname+"/build/index.html"))
+})*/
+
+app.get("/:sala",(req,res)=>{
+
+	res.sendFile(__dirname+"/build/index.html")
+})
+
+var http=require("http")
+var server = http.createServer(app)
+var io = require('socket.io')(server)
+
+app.set('port', 8080)
+
+sanitizeString = (str) => {
+	return xss(str)
+}
+
+connections = {}
+messages = {}
+timeOnline = {}
+
+io.on('connection', (socket) => {
+	console.log("connectado")
+
+	socket.on('join-call', (path) => {
+		if(connections[path] === undefined){
+			connections[path] = []
+		}
+		connections[path].push(socket.id)
+
+		timeOnline[socket.id] = new Date()
+
+		for(let a = 0; a < connections[path].length; ++a){
+			io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
+		}
+
+		if(messages[path] !== undefined){
+			for(let a = 0; a < messages[path].length; ++a){
+				io.to(socket.id).emit("chat-message", messages[path][a]['data'], 
+					messages[path][a]['sender'], messages[path][a]['socket-id-sender'])
+			}
+		}
+
+		console.log(path, connections[path])
+	})
+
+	socket.on('signal', (toId, message) => {
+		io.to(toId).emit('signal', socket.id, message)
+	})
+
+	socket.on('chat-message', (data, sender) => {
+		data = sanitizeString(data)
+		sender = sanitizeString(sender)
+
+		var key
+		var ok = false
+		for (const [k, v] of Object.entries(connections)) {
+			for(let a = 0; a < v.length; ++a){
+				if(v[a] === socket.id){
+					key = k
+					ok = true
+				}
+			}
+		}
+
+		if(ok === true){
+			if(messages[key] === undefined){
+				messages[key] = []
+			}
+			messages[key].push({"sender": sender, "data": data, "socket-id-sender": socket.id})
+			console.log("message", key, ":", sender, data)
+
+			for(let a = 0; a < connections[key].length; ++a){
+				io.to(connections[key][a]).emit("chat-message", data, sender, socket.id)
+			}
+		}
+	})
+
+	socket.on('disconnect', () => {
+		var diffTime = Math.abs(timeOnline[socket.id] - new Date())
+		var key
+		for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
+			for(let a = 0; a < v.length; ++a){
+				if(v[a] === socket.id){
+					key = k
+
+					for(let a = 0; a < connections[key].length; ++a){
+						io.to(connections[key][a]).emit("user-left", socket.id)
+					}
+			
+					var index = connections[key].indexOf(socket.id)
+					connections[key].splice(index, 1)
+
+					console.log(key, socket.id, Math.ceil(diffTime / 1000))
+
+					if(connections[key].length === 0){
+						delete connections[key]
+					}
+				}
+			}
+		}
+	})
+})
+
+server.listen(app.get('port'), () => {
+	console.log("listening on", app.get('port'))
+})
