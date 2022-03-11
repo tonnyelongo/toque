94abaab20d690659fae35f493459cbcf4a617a91
tonnyelongo <77832721+tonnyelongo@users.noreply.github.com>
2022-03-12 00:21:12 +0100

    Add files via upload

---
+++ build/precache-manifest.45ffac6829308222b0b8f164d7074f93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c0a2ce53240c4d19615b95fb71d39e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "674ba624cc13b2b9a618",
+    "url": "./static/css/2.b1080609.chunk.css"
+  },
+  {
+    "revision": "6fee1f452c1ab222eb6f",
+    "url": "./static/css/main.7e169c8b.chunk.css"
+  },
+  {
+    "revision": "674ba624cc13b2b9a618",
+    "url": "./static/js/2.12762b75.chunk.js"
+  },
+  {
+    "revision": "71e9e4dedda4f381ea56e80067d4fca8",
+    "url": "./static/js/2.12762b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fee1f452c1ab222eb6f",
+    "url": "./static/js/main.9dd3dd2e.chunk.js"
+  },
+  {
+    "revision": "54556fe9f472d1cbeaa5",
+    "url": "./static/js/runtime-main.7c3abc12.js"
+  }
+]);
